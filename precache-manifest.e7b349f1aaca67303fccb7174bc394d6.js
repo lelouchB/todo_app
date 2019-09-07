1959bd801058875527894230066a92f43dd55538
Ashutosh Kumar Singh <ashusingh15673@gmail.com>
2019-09-07 23:29:10 +0530

    Updates

---
+++ precache-manifest.e7b349f1aaca67303fccb7174bc394d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113604445286cf9fa82e2f3c73454dcf",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "39085c516e95850df8db",
+    "url": "/todo-app/static/css/main.717115b9.chunk.css"
+  },
+  {
+    "revision": "184ca6a9e4fca1244176",
+    "url": "/todo-app/static/js/2.e03b8c3b.chunk.js"
+  },
+  {
+    "revision": "39085c516e95850df8db",
+    "url": "/todo-app/static/js/main.b1e51b2f.chunk.js"
+  },
+  {
+    "revision": "7fc361c648dbb354bac8",
+    "url": "/todo-app/static/js/runtime~main.0655b730.js"
+  }
+]);
