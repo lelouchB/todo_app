f64ed768c7915c3541e05dc20bded205380622b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-02 14:17:50 +0000

    Deploy lelouchB/todo_app to github.com/lelouchB/todo_app.git:gh-pages

---
+++ precache-manifest.01ccee09fe1b83753a8488fe3d731b9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7e5f5ff15cfb526b055b6e97a1925b",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "a85706664755ed17d56b",
+    "url": "/todo_app/static/css/main.5426a790.chunk.css"
+  },
+  {
+    "revision": "184ca6a9e4fca1244176",
+    "url": "/todo_app/static/js/2.e03b8c3b.chunk.js"
+  },
+  {
+    "revision": "a85706664755ed17d56b",
+    "url": "/todo_app/static/js/main.629d38b0.chunk.js"
+  },
+  {
+    "revision": "a14e9dcf60dc019cb2a5",
+    "url": "/todo_app/static/js/runtime~main.f5edb76e.js"
+  }
+]);
